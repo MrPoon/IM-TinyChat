fc8bf84da5d751ee644b8a5c01840ce243671bd1
潘阳君 <panyangjun1@panyangjun.local>
2015-06-07 23:52:54 +0800

    Initial Commit

---
+++ 微聊/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微聊
+//
+//  Created by 潘阳君 on 15/6/7.
+//  Copyright (c) 2015年 潘阳军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微聊/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  微聊
+//
+//  Created by 潘阳君 on 15/6/7.
+//  Copyright (c) 2015年 潘阳军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
